f48ef968d42b209d0b046e1551cc0980f7f57483
digiwiz1 <ruud.righarts@gmail.com>
2015-09-23 14:38:01 +0200

    1th commit

---
+++ app/src/main/java/com/digiwiz/geofence/Constants.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.digiwiz.geofence;
+
+public final class Constants {
+
+    public static final String KEY_POWER_MODE = "com.digiwiz.geofence.KEY_POWER_MODE";
+    public static final String KEY_UPDATE_INTERVAL = "com.digiwiz.geofence.KEY_UPDATE_INTERVAL";
+
+    public static final String KEY_SHOW_COORDINATES_NOTIFICATION = "com.digiwiz.geofence.KEY_SHOW_COORDINATES_NOTIFICATION";
+
+    public static final String KEY_SHOW_GEOFENCE_NOTIFICATION = "com.digiwiz.geofence.KEY_SHOW_GEOFENCE_NOTIFICATION";
+
+    public static final String KEY_ENTER_GEOFENCE_URL = "com.digiwiz.geofence.KEY_ENTER_GEOFENCE_URL";
+    public static final String KEY_EXIT_GEOFENCE_URL = "com.digiwiz.geofence.KEY_EXIT_GEOFENCE_URL";
+    public static final String LOG_TAG = "GeoFence";
+    // Keys for geofence data stored in SharedPreferences.
+    public static final String KEY_LATITUDE = "com.digiwiz.geofence.KEY_LATITUDE";
+    public static final String KEY_LONGITUDE = "com.digiwiz.geofence.KEY_LONGITUDE";
+    public static final String KEY_RADIUS = "com.digiwiz.geofence.KEY_RADIUS";
+    public static final String KEY_TRACK_COORDINATES = "com.digiwiz.geofence.KEY_TRACK_COORDINATES";
+
+    private Constants() {
+    }
+}
---
+++ app/src/main/java/com/digiwiz/geofence/settings/SettingsFragment.java
@@ -0,0 +1,25 @@
+package com.digiwiz.geofence.settings;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+import com.digiwiz.geofence.R;
+
+/**
+ * The setting fragment that shows the preferences for the application.
+ */
+public class SettingsFragment extends PreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Load the preferences as configured in the /res/xml/preferences.xml file
+        // and displays them.
+        // The preferences will be automatically saved.
+        addPreferencesFromResource(R.xml.preferences);
+    }
+
+
+}
+
